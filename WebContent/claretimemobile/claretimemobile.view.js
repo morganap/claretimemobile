f4842e7db986400134d1482dc80fec96ae3ed682
Morgan Apelqvist <morgan.apelqvist@gmail.com>
2013-03-14 11:11:42 +0100

    test

---
+++ WebContent/claretimemobile/claretimemobile.view.js
@@ -0,0 +1,15 @@
+sap.ui.jsview("claretimemobile.claretimemobile", {
+
+      getControllerName : function() {
+         return "claretimemobile.claretimemobile";
+      },
+
+      createContent : function(oController) {
+          return new sap.m.Page({
+              title: "Title",
+              content: [
+              ]
+          });
+      }
+
+});
